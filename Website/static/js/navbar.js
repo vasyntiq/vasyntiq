e9fc2200e637112610f6d9bee4be084d29d67e38
vasyntiq <vasyntiq@gmail.com>
2026-06-15 15:44:08 +0530

    Add files via upload

---
+++ Website/static/js/navbar.js
@@ -0,0 +1,34 @@
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    () => {
+
+        const toggle =
+
+            document.querySelector(
+
+                ".menu-toggle"
+            );
+
+        const navLinks =
+
+            document.querySelector(
+
+                ".nav-links"
+            );
+
+        toggle.addEventListener(
+
+            "click",
+
+            () => {
+
+                navLinks.classList.toggle(
+
+                    "active"
+                );
+            }
+        );
+    }
+);
